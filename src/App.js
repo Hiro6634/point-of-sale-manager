df2e036c4985771312fe5072faa49c643d782605
Hiro <hiro.suyama@gmail.com>
2022-09-24 11:25:11 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+const  App = () => {
+  return (
+    <div className="product-container">
+      <div className="product-header-container">
+        <span>Categoria</span>
+        <span>Producto</span>
+        <span>Precio</span>
+        <span>Habilitado</span>
+        <span>Editar</span>
+        <span>Borrar</span>
+      </div>
+      <div className="stock-container">
+        <span>Parrilla</span>
+        <span>Hamburguesa</span>
+        <span>$100</span>
+        <span>true</span>
+        <span>Edit</span>
+        <span>Del</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
