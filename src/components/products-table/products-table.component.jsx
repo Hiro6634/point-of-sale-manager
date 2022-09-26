f1528ae03c20d19f642d9ec9b7dd962e3a0e8221
Hiro <hiro.suyama@gmail.com>
2022-09-25 23:56:09 -0300

    Products Table

---
+++ src/components/products-table/products-table.component.jsx
@@ -0,0 +1,9 @@
+const ProductsTable = (products) => {
+    return(
+        <div>
+            <H1>I'm Products Table</H1>
+        </div>
+    );
+};
+
+export default ProductsTable;
--- src/routes/products/products.component.jsx
+++ src/routes/products/products.component.jsx
@@ -5,15 +5,29 @@ import { ProductsContext } from '../../context/products.context';
 const Products = () => {
     const {products} = useContext(ProductsContext);
     return(
-        <div>
+        <div className='products-container'>
             <h2>Products List</h2>
-            {
-                products.map(({id, name}) => (
-                    <div key = {id}>
-                        <h2>{name}</h2>
-                    </div>
-                ))
-            }
+            <div className='products-table-container'>
+                <h2>I'm Products Table</h2>
+                {/* <div className='products-hdr-container'>
+                    <span className='product-header'>Categoria</span>
+                    <span className='product-header'>Producto</span>
+                    <span className='product-header'>Precio</span>
+                    <span className='product-header'>Habilitado</span>
+                    <span className='product-header'>Editar</span>
+                    <span className='product-header'>Borrar</span>
+                </div>
+                {
+                    products.map((product) => {
+                        const {id, category, name, price, enable} = product;
+                        return(
+                        <div key = {id} className='products-body-container'>
+                            <span>{category}</span>
+                            <span>{name}</span>
+                        </div>
+                    )})
+                } */}
+            </div>
         </div>
     );
 };
