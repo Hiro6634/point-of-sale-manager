277bdb33509272912bec7497f5e7293ec8162458
Hiro Suyama <hiro.suyama@gmail.com>
2024-09-19 01:28:49 -0300

    formatting Products Table

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -6,25 +6,25 @@ const ProductsTable = () => {
     return (
         <div className="w-full flex flex-col items-start">
             <table className="w-full">
-                <thead className="bg-black text-white border-b-2">
+                <thead className="bg-gray-500 border-b-2">
                     <tr className="columns-1">
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left text-silver-500">Categoria</th>
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left">Producto</th>
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left">Precio</th>
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left">Stock</th>
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left">Habilitado</th>
-                        <th className="p-2 text-sm font-semibold tracking-wide text-left">Acciones</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Categoria</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Producto</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Precio</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Stock</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Habilitado</th>
+                        <th className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-700 uppercase">Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
                     {products.map((product) => (
                         <tr key={product.id} className="p-2 columns-1">
-                            <td className="p-2 text-sm font-bold tracking-wide text-left">{product.category}</td>
-                            <td className="p-2 text-sm font-mono tracking-wide text-left">{product.name}</td>
-                            <td className="p-2 text-sm font-mono tracking-wide text-right">{product.price}</td>
-                            <td className="p-2 text-sm font-mono tracking-wide text-right">{product.stock}</td>
-                            <td className="p-2 text-sm font-mono tracking-wide text-left">{product.enable}</td>
-                            <td className="p-2 text-sm font-mono tracking-wide text-left">{product.enable}</td>
+                            <td className="p-2 text-sm font-bold tracking-wide text-left border border-gray-400">{product.category}</td>
+                            <td className="p-2 text-sm font-mono tracking-wide text-left border border-gray-400">{product.name}</td>
+                            <td className="p-2 text-sm font-mono tracking-wide text-right border border-gray-400">{product.price}</td>
+                            <td className="p-2 text-sm font-mono tracking-wide text-right border border-gray-400">{product.stock}</td>
+                            <td className="p-2 text-sm font-mono tracking-wide text-left border border-gray-400">{product.enable}</td>
+                            <td className="p-2 text-sm font-mono tracking-wide text-left border border-gray-400">{product.enable}</td>
                         </tr>
                     ))}
 
