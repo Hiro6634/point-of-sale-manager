ef9ef0182c5b8066eaa1e46c3dc35d705a8e625e
Hiro Suyama <hiro.suyama@gmail.com>
2024-09-29 22:43:18 -0300

    Continue to make product table editable

--- src/components/ProductsTableLine.jsx
+++ src/components/ProductsTableLine.jsx
@@ -45,19 +45,28 @@ const ProductsTableLine = ({ product }) => {
                 {enableEdit ?
                     <input className="p-2 text-sm font-semibold tracking-wide text-left border border-gray-400 uppercase" type="text" name="name" value={productState.name} onChange={handelChange} />
                     :
-                    <label>{productState.name}</label>}
+                    <label>{productState.name}</label>
+                }
             </td>
             <td className="p-2 text-sm font-semibold tracking-wide text-right border border-gray-400 uppercase">${product.price}</td>
             <td className="p-2 text-sm font-semibold tracking-wide text-right border border-gray-400 uppercase">{product.stock}    </td>
             <td className="p-2 text-sm font-semibold tracking-wide text-center border border-gray-400 uppercase w-1/12">
                 <div className="flex space-x-2 justify-center" onClick={() => handleToggle(product)}>
-                    {product.enable ? <HiOutlineCheck className="text-green-500" /> : <HiOutlineBan className="text-red-500" />}
+                    {product.enable ?
+                        <HiOutlineCheck style={{ fontSize: "24px" }} className="text-green-500" />
+                        :
+                        <HiOutlineBan style={{ fontSize: "24px" }} className="text-red-500" />
+                    }
                 </div>
             </td>
             <td className="p-2 text-sm font-mono tracking-wide text-left border border-gray-400 w-1/12">
                 <div className="flex space-x-2 justify-center">
-                    {enableEdit ? <HiOutlineCheckCircle onClick={() => setEnableEdit(false)} /> : <HiOutlinePencil onClick={() => setEnableEdit(true)} />}
-                    <HiOutlineTrash onClick={() => handleDelete(product)} />
+                    {enableEdit ?
+                        <HiOutlineCheckCircle style={{ fontSize: "24px" }} className="text-green-500" onClick={() => setEnableEdit(false)} />
+                        :
+                        <HiOutlinePencil style={{ fontSize: "24px" }} className="text-blue-500" onClick={() => setEnableEdit(true)} />
+                    }
+                    <HiOutlineTrash style={{ fontSize: "24px" }} className="text-red-500" onClick={() => handleDelete(product)} />
                 </div>
             </td>
         </tr>
